03f0ed45be0e6a89f7f45c19636e573226d6d955
hyounakura <leopardtender@gmail.com>
2020-07-10 08:34:34 +0900

    RspecTest

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :group_users
   has_many :groups, through: :group_users
   has_many :comments
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: {case_sensitive: true}
   
 end
